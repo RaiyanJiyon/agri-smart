4a5526a66e81c54992db98b991a9274dc3b69aad
RaiyanJiyon <raiyanjiyon814@gmail.com>
2025-04-09 10:31:10 +0600

    feat: Added why-choose-us.tsx and integrate inside home.tsx

--- src/components/home-page/home.tsx
+++ src/components/home-page/home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroSlider from './hero-slider';
+import WhyChooseUs from './why-choose-us';
 
 const HomePage = () => {
     return (
         <div>
             <HeroSlider />
+            <WhyChooseUs />
         </div>
     );
 };
