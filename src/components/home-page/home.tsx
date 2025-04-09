85b1ca2f94d72e25d9106fce02d4ba553eae10e5
RaiyanJiyon <raiyanjiyon814@gmail.com>
2025-04-09 10:39:14 +0600

    feat: Added how-works.tsx and integrate inside home.tsx

--- src/components/home-page/home.tsx
+++ src/components/home-page/home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroSlider from './hero-slider';
 import WhyChooseUs from './why-choose-us';
+import HowItWorks from './how-works';
 
 const HomePage = () => {
     return (
         <div>
             <HeroSlider />
             <WhyChooseUs />
+            <HowItWorks />
         </div>
     );
 };
