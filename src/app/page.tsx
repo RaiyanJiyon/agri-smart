93c04e425bf58a5bd27813cf4b0ea457acc9a0c8
RaiyanJiyon <raiyanjiyon814@gmail.com>
2025-04-11 12:25:56 +0600

    resolve: remove home.tsx and include every home-page components inside main page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
-import HomePage from "@/components/home-page/home";
+import CallToAction from "@/components/home/call-to-action";
+import Featured from "@/components/home/featured";
+import HeroSlider from "@/components/home/hero-slider";
+import HowItWorks from "@/components/home/how-works";
+import Stats from "@/components/home/stats";
+import Testimonials from "@/components/home/testimonials";
+import WhyChooseUs from "@/components/home/why-choose-us";
 
 export default function Home() {
-  return <div className="">
-    <HomePage />
-  </div>;
+  return (
+    <div className="">
+      {/* Hero Slider Section */}
+      <HeroSlider />
+
+      {/* Benefits Section */}
+      <WhyChooseUs />
+
+      {/* How It Works Section */}
+      <HowItWorks />
+
+      {/* Stats Section */}
+      <Stats />
+
+      {/* Testimonials Section */}
+      <Testimonials />
+
+      {/* Featured In Section */}
+      <Featured />
+
+      {/* CTA Section */}
+      <CallToAction />
+    </div>
+  );
 }
