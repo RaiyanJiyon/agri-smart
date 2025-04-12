6699e5ead6684dd58c0758184c68adc126d43ffb
RaiyanJiyon <raiyanjiyon814@gmail.com>
2025-04-12 12:07:23 +0600

    feat: Added products GET api routes for getting specific product

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,7 +1,38 @@
 // app/api/products/[id]/route.ts
 import { connectToDB } from "@/lib/mongoose/connect";
 import { Product } from "@/models/product.model";
+import { Types } from "mongoose";
 import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  await connectToDB();
+
+  const { id } = params;
+
+  // Validate MongoDB ObjectId
+  if (!Types.ObjectId.isValid(id)) {
+    return NextResponse.json({ error: "Invalid product ID" }, { status: 400 });
+  }
+
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return NextResponse.json({ error: "Product not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(product, { status: 200 });
+  } catch (error) {
+    console.error("GET Product Error:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
 
 export async function PATCH(
   req: NextRequest,
